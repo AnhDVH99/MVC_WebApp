d3851d24a43440587a625734364ff9040946a4d1
ASUS <ASUS@STEVENA>
2024-10-14 10:05:59 +0700

    Change title and validate login

--- Validators/LoginViewModelValidator.cs
+++ Validators/LoginViewModelValidator.cs
@@ -21,6 +21,18 @@ namespace ASP.NET_Core_MVC_Piacom.Validators
             RuleFor(u => u.Password)
                 .NotEmpty().WithMessage("Please enter password");
 
+            RuleFor(u => new { u.Username, u.Password })
+                .Must(login => ValidateUserCredentials(login.Username, login.Password))
+                .WithMessage("Invalid username or password");
+
+        }
+        private bool ValidateUserCredentials(string username, string password)
+        {
+            var user = _userManager.FindByNameAsync(username).Result;
+            if (user == null) return false;
+
+            var validPassword = _userManager.CheckPasswordAsync(user, password).Result;
+            return validPassword;
         }
     }
 }
